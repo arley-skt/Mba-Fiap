e14202bc2d4b2dd89158920f142e02d562c64f4f
arley.ferreira <arley.ferreira@itlean.com.br>
2022-10-07 15:40:27 -0300

    models serviço identidade e pedido

---
+++ TrabMba/src/services/Mba.Identidade.API/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using Mba.Identidade.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mba.Identidade.API.Controllers
+{
+    [ApiController]
+    [Route("api/identidade")]
+    public class AuthController: Controller
+    {        
+        [HttpPost("Registrar")]
+        public  ActionResult Registrar(UsuarioRegistro user)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            return Ok();
+        }
+        [HttpPost("Autenticar")]
+        public ActionResult Login(UsuarioLogin user)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            return Ok();
+        }
+    }
+}
---
+++ TrabMba/src/services/Mba.Identidade.API/Models/UsuarioRegistro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mba.Identidade.API.Models
+{
+    public class UsuarioRegistro
+    {
+        [Required]
+        public string email { get; set; }
+        [Required]
+        public string senha { get; set; }
+        [Required]
+        public string Confirmacaosenha { get; set; }
+    }
+}
